2e4445fbad52ceb548b7a40ec37f933de244fbc9
delare <emmalito.delar@grenoble-inp.org>
2022-07-24 16:50:04 +0200

    retrait des threads

--- src/Steps/step7_UCB1.py
+++ src/Steps/step7_UCB1.py
@@ -5,7 +5,6 @@
 
 import sys
 sys.path.append("./src")
-import threading
 from Simulator.Environment import bigEnvironment
 from Learner.learner import UCB1
 from Algorithme import optimization
@@ -15,32 +14,6 @@ from Algorithme import optimization
 nbCustomer = 1000
 nbDays = 20
 
-
-def learn(learners, l_alphas, l_nbItem, simulator, products, nbItemMax):
-    """Single context that would be run in parallel """
-    for _ in range(nbDays):
-        #We choose the arm to pull
-        pulled_arm = [learner.pull_arm() for learner in learners]
-        pulled_arm_alpha = l_alphas.pull_arm()
-        pulled_arm_item = l_nbItem.pull_arm()
-
-        #We change the price in function
-        for index in range(len(products)):
-            products[index].changePrice(pulled_arm[index])
-
-        #We get the corresponding rewards
-        cart, rewards1, entrance, nbImpress = simulator.runDay(nbCustomer)
-        nbImpress = list(nbImpress.values())
-        rewards1 = list(rewards1.values())
-        rewards0 = [nbImpress[i] - rewards1[i] for i in range(len(rewards1))]
-
-        #We update the distribution
-        for index in range(len(learners)):
-            learners[index].update(pulled_arm[index], rewards0[index], rewards1[index])
-        l_alphas.update(pulled_arm_alpha, nbCustomer - entrance[pulled_arm_alpha], entrance[pulled_arm_alpha])
-        if cart[pulled_arm_item] != 0:
-            l_nbItem.update(pulled_arm_item, nbItemMax*rewards1[pulled_arm_item]-cart[pulled_arm_item],
-                            cart[pulled_arm_item])
 
 
 def getRates(simulators, products, nbItemsMax):
@@ -54,26 +27,45 @@ def getRates(simulators, products, nbItemsMax):
     #Learner for the nb Item sold per product per price
     allL_nbItem = [UCB1(5) for  _ in range(len(simulators))]
 
-    threads = []
-    for index in range(len(simulators)):
-        x = threading.Thread(target=learn, args=(allLearners[index], allL_alphas[index],
-                            allL_nbItem[index], simulators[index], products.copy(), nbItemsMax[index],))
-        x.start()
-        threads.append(x)
-    
-    for thread in threads:
-        thread.join()
+    for i in range(len(simulators)):
+        learners = allLearners[i]
+        l_alphas = allL_alphas[i]
+        l_nbItem = allL_nbItem[i]
+        simulator = simulators[i]
+        nbItemMax = nbItemsMax[i]
+
+        for _ in range(nbDays):
+            #We choose the arm to pull
+            pulled_arm = [learner.pull_arm() for learner in learners]
+            pulled_arm_alpha = l_alphas.pull_arm()
+            pulled_arm_item = l_nbItem.pull_arm()
+
+            #We change the price in function
+            for index in range(len(products)):
+                products[index].changePrice(pulled_arm[index])
+
+            #We get the corresponding rewards
+            cart, rewards1, entrance, nbImpress = simulator.runDay(nbCustomer)
+            nbImpress = list(nbImpress.values())
+            rewards1 = list(rewards1.values())
+            rewards0 = [nbImpress[i] - rewards1[i] for i in range(len(rewards1))]
+
+            #We update the distribution
+            for index in range(len(learners)):
+                learners[index].update(pulled_arm[index], rewards0[index], rewards1[index])
+            l_alphas.update(pulled_arm_alpha, nbCustomer - entrance[pulled_arm_alpha], entrance[pulled_arm_alpha])
+            if cart[pulled_arm_item] != 0:
+                l_nbItem.update(pulled_arm_item, nbItemMax*rewards1[pulled_arm_item]-cart[pulled_arm_item],
+                                cart[pulled_arm_item])
 
     convRates = {}
-    index, idx = 0, 0
+    index = 0
     for learners in allLearners:
-        convRates[index] = [[]]*5
+        convRates[index] = []
         for learner in learners:
             expected = learner.getExpectedPayoff()
-            convRates[index][idx].append([elem for elem in expected])
-            idx += 1
+            convRates[index].append([elem for elem in expected])
         index += 1
-        idx = 0
 
     alphas = []
     for l_alphas in allL_alphas:
@@ -84,7 +76,6 @@ def getRates(simulators, products, nbItemsMax):
     nbItemSold = []
     for l_nbItem in allL_nbItem:
         expected = l_nbItem.getExpectedPayoff()
-        print("For ", index, ": ", expected)
         nbItemSold.append([elem * nbItemsMax[index] for elem in expected])
         index += 1
 
@@ -99,17 +90,20 @@ if __name__ == "__main__":
     #1 - We learn the conversion rates
     conversionRates, alphas, nbItemSold = getRates(simulators, products, nbItemsMax)
 
-    print(nbItemSold)
-
     #2 - We fix the others parameters
-    #margin = [product.getPrices() for product in products] #We fix the price as the margin
-    #totalUsers = nbCustomer * nbDays
-    #graphWeights = [0.2,0.3,0.2,0.3]
-#
-    ##3 - We play the algorithm
-    #bestPrices, bestTotalmargin, _ = optimization(margin, conversionRates, alphas,
-    #                                            nbItemSold, totalUsers, graphWeights)
-    #for idx in range(len(bestPrices)):
-    #    print("For the product ", idx, " the best price is ",
-    #          margin[idx][bestPrices[idx]])
-    #print("The total margin with this configuration is ", bestTotalmargin)
+    margin = [product.getPrices() for product in products] #We fix the price as the margin
+    totalUsers = nbCustomer * nbDays
+    graphWeights = [[[0.7, 0.6, 0.9, 0.6, 0.2], [0.4, 0.2, 0.1, 0.5, 0.1]],
+                    [[0.3, 0.2, 0.6, 0.8, 0.6], [0.4, 0.2, 0.4, 0.7, 0.3]],
+                    [[0.5, 0.4, 0.2, 0.6, 0.7],  [0.5, 0.4, 0.1, 0.5, 0.5]]]
+
+    #3 - We play the algorithm
+    for index in range(len(conversionRates)):
+        
+        bestPrices, bestTotalmargin, _ = optimization(margin, conversionRates[index], alphas[index],
+                                                    nbItemSold[index], totalUsers, graphWeights[index])
+        print("For the user class ", index, " we have:")
+        for idx in range(len(bestPrices)):
+            print("For the product ", idx, " the best price is ",
+                margin[idx][bestPrices[idx]])
+        print("The total margin with this configuration is ", bestTotalmargin, "\n")
